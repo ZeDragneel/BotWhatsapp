8b8d813718f52f4ba3adce9f01f56eebd3ce9d46
José <84270806+Jose21NC@users.noreply.github.com>
2022-06-28 08:48:15 -0600

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -2,16 +2,16 @@ import fs from 'fs'
 function handler(m, { conn }) {
 let text = `
 *𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝘾𝙤𝙣𝙩𝙖𝙘𝙩* 
-*Wa.me/593993684821 (BOT)*
-*Wa.me/51993042301 (BOT)*
-*https://www.instagram.com/gata_dios*
+*Wa.me/50576796164 (Owner)*
+*Wa.me/50578229009 (BOT)*
+*https://www.instagram.com/josedev2nc*
 `.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
-body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
+title: '𝗦𝗛𝗜𝗡𝗢𝗔𝜷𝛔𝞽 | 𝗝𝗼𝘀𝗲𝗭𝘇𝘇',
+body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
 previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
-sourceUrl: `https://wa.me/593993684821`}}})
+sourceUrl: `https://wa.me/50576796164`}}})
   
 //const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 //this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
@@ -20,7 +20,7 @@ let pp = './media/menus/Menu2.jpg'
 let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
 
 conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/gata_dios', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['🐈 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
+['🌠 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
 ['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
 ['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
 ], m,)
